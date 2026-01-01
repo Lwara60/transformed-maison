6926415f7c682051857a3e5253becf4da7bf2e89
Lwando Tinzi <msuthuxhakaza@gmail.com>
2026-01-01 19:29:12 +0200

    chore: add Vercel SPA rewrite and set Vite base to './'

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true
-  }
+  base: './' // Serve assets with relative paths (fixes blank page on Vercel for some setups)
 })
