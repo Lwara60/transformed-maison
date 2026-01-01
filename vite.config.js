cce1c60d8a9bb993ca0a387894f78e7bc8626d16
Lwando Tinzi <msuthuxhakaza@gmail.com>
2026-01-01 20:22:50 +0200

    Update vite.config.js: use React plugin and set base to '/'

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './' // Serve assets with relative paths (fixes blank page on Vercel for some setups)
+  base: '/' // Serve assets from root (Vercel host root)
 })
